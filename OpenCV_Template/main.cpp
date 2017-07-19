ffbd5bdd328db238c8c178acf3ea2f638a4f80b4
William Barbosa <williampmb@gmail.com>
2017-07-19 08:46:35 -0300

    Opencv basics 5

--- OpenCV_Template/main.cpp
+++ OpenCV_Template/main.cpp
@@ -7,24 +7,18 @@ using namespace std;
 int main(int arg, char** argc)
 {
 	Mat original = imread("test.JPG", CV_LOAD_IMAGE_COLOR);
-	Mat modified = imread("test.JPG", CV_LOAD_IMAGE_COLOR);
-
-	//Iterate through the img. 
-	for (int r = 0; r < modified.rows;r++)
-	{
-		for (int c = 0 ; c < modified.cols; c++)
-		{
-			//GrayScale Img
-			//modified.at<uint8_t>(r, c) = modified.at<uint8_t>(r,c) * 0.5f;
-
-			//Color Img. Vec3b = 3 band, b = byte
-			modified.at<cv::Vec3b>(r, c)[2] = modified.at<cv::Vec3b>(r, c)[2] * 0.0f;
-		}
-	}
 	
-	imshow("Original", original);
-	imshow("Modified", modified);
+	Mat sptChannels[3];
 
+	split(original, sptChannels);
+
+	sptChannels[2] = Mat::zeros(sptChannels[2].size(), CV_8UC1);
+
+	Mat output;
+
+	merge(sptChannels, 3, output);
+
+	imshow("Merged", output);
 
 	waitKey();
 }
