8d6b482afca6c506a2ef114ec38ed3f74e2fc34a
William Barbosa <williampmb@gmail.com>
2017-07-19 14:26:06 -0300

    OpenCv 6 e 7

--- OpenCV_Template/main.cpp
+++ OpenCV_Template/main.cpp
@@ -4,21 +4,54 @@
 using namespace cv;
 using namespace std;
 
-int main(int arg, char** argc)
+void takeDFT(Mat& source, Mat& dest)
 {
-	Mat original = imread("test.JPG", CV_LOAD_IMAGE_COLOR);
-	
-	Mat sptChannels[3];
+	Mat originalComplex[2] = { source, Mat::zeros(source.size(), CV_32F) };
 
-	split(original, sptChannels);
+	Mat dftReady;
 
-	sptChannels[2] = Mat::zeros(sptChannels[2].size(), CV_8UC1);
+	merge(originalComplex, 2, dftReady);
 
-	Mat output;
+	Mat dftOfOriginal;
 
-	merge(sptChannels, 3, output);
+	dft(dftReady, dftOfOriginal, DFT_COMPLEX_OUTPUT);
 
-	imshow("Merged", output);
+	dest = dftOfOriginal;
+}
+
+void showDFT(Mat& source)
+{
+	Mat splitArray[2] = { Mat::zeros(source.size(),CV_32F), Mat::zeros(source.size(),CV_32F) };
+
+	split(source, splitArray);
+
+	Mat dftMagnitude;
+
+	magnitude(splitArray[0], splitArray[1], dftMagnitude);
+
+	dftMagnitude += Scalar::all(1);
+
+	log(dftMagnitude, dftMagnitude);
+
+	normalize(dftMagnitude, dftMagnitude, 0, 1, CV_MINMAX);
+
+	imshow("DFT", dftMagnitude);
 
 	waitKey();
+}
+
+int main(int arg, char** argc)
+{
+	Mat original = imread("test.JPG", CV_LOAD_IMAGE_GRAYSCALE);
+
+	Mat originalFloat;
+
+	// 1/255 = normalized
+	original.convertTo(originalFloat, CV_32FC1, 1.0 / 255.0);
+
+	Mat dftOfOriginal;
+
+	takeDFT(originalFloat, dftOfOriginal);
+
+	showDFT(dftOfOriginal);
 }
