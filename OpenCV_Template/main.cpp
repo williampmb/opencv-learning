e1e8520e43895cc528cb121eaa5593a5698ffbf4
William Barbosa <williampmb@gmail.com>
2017-07-19 08:25:41 -0300

    Opencv Basics 3

--- OpenCV_Template/main.cpp
+++ OpenCV_Template/main.cpp
@@ -4,13 +4,20 @@ using namespace cv;
 
 int main(int arg, char** argc)
 {
-	Mat test = imread("test.JPG",CV_LOAD_IMAGE_COLOR);
-	Mat tstGray = imread("test.JPG", CV_LOAD_IMAGE_GRAYSCALE);
-	
-	imshow("tst", test);
-	imshow("gray", tstGray);
+	Mat tst1 = imread("test.JPG", CV_LOAD_IMAGE_UNCHANGED);
+	Mat tst2 = imread("test.JPG", CV_LOAD_IMAGE_GRAYSCALE);
 
-	imwrite("outputGray.jpg", tstGray);
+	namedWindow("Color", CV_WINDOW_FREERATIO);
+	namedWindow("Fixed", CV_WINDOW_AUTOSIZE);
+
+	imshow("Color", tst1);
+	imshow("Fixed", tst2);
+
+	resizeWindow("Color", tst1.cols, tst1.rows);
+	resizeWindow("Fixed", tst2.cols/2, tst2.rows/2);
+
+	moveWindow("Color", 1000, 600);
+	moveWindow("Fixed", 1000 + tst1.cols, 800);
 
 	waitKey();
 }
