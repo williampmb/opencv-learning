6faa7ff1e2d4151d35c11b95669b4f25b764eb60
William Barbosa <williampmb@gmail.com>
2017-07-19 08:08:05 -0300

    OpenCV Basics 2

--- OpenCV_Template/main.cpp
+++ OpenCV_Template/main.cpp
@@ -4,7 +4,13 @@ using namespace cv;
 
 int main(int arg, char** argc)
 {
-	Mat test = imread("test.JPG",CV_LOAD_IMAGE_UNCHANGED);
-	imshow("tst",test);
+	Mat test = imread("test.JPG",CV_LOAD_IMAGE_COLOR);
+	Mat tstGray = imread("test.JPG", CV_LOAD_IMAGE_GRAYSCALE);
+	
+	imshow("tst", test);
+	imshow("gray", tstGray);
+
+	imwrite("outputGray.jpg", tstGray);
+
 	waitKey();
 }
