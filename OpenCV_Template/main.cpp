17d0d7e141ac57005d6b220a2457f508831f317e
William Barbosa <williampmb@gmail.com>
2017-07-21 09:21:10 -0300

    Opencv Basics 15 - Aruco Markers

--- OpenCV_Template/main.cpp
+++ OpenCV_Template/main.cpp
@@ -1,32 +1,34 @@
-#include "opencv2\opencv.hpp"
+#include "opencv2\core.hpp"
+#include "opencv2\imgcodecs.hpp"
+#include "opencv2\imgproc.hpp"
+#include "opencv2\highgui.hpp"
+#include "opencv2\aruco.hpp"
+#include "opencv2\calib3d.hpp"
+
 #include <stdint.h>
 
 using namespace cv;
 using namespace std;
 
+void createArucoMarkers()
+{
+	Mat outputMarker;
+
+	Ptr<aruco::Dictionary> markerDictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
+
+	for (int i = 0; i < 50; i++)
+	{
+		aruco::drawMarker(markerDictionary, i, 500, outputMarker, 1);
+		ostringstream convert;
+		string imageName = "4x4Marker_";
+		convert << imageName << i << ".jpg";
+		imwrite(convert.str(),outputMarker);
+
+	}
+}
+
 int main(int arg, char** argc)
 {
+	createArucoMarkers();
 
-	VideoCapture vid("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov");
-
-	if (!vid.isOpened())
-	{
-		return -1;
-	}
-
-	namedWindow("Webcam", CV_WINDOW_AUTOSIZE);
-
-	while (char(waitKey(1)) != 'q' && vid.isOpened())
-	{
-		Mat frame;
-		
-		vid >> frame;
-		if (frame.empty())
-		{
-			cout << "Video Over" << endl;
-		}
-		imshow("webcam", frame);
-
-	}
-	return 1;
 }
