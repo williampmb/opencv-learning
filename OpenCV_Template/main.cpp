e4914b3ed7259f1416b1cd25839a475274c257b1
William Barbosa <williampmb@gmail.com>
2017-07-21 10:59:44 -0300

    Opencv Basics 16,17 - camera calibration

--- OpenCV_Template/main.cpp
+++ OpenCV_Template/main.cpp
@@ -10,6 +10,10 @@
 using namespace cv;
 using namespace std;
 
+const float calibrationSquareDimension = 0.0255f; //metters
+const float arucoSquareDimension = 0.19f; //metters
+const Size chessboardDimensions = Size(9, 6);
+
 void createArucoMarkers()
 {
 	Mat outputMarker;
@@ -22,13 +26,85 @@ void createArucoMarkers()
 		ostringstream convert;
 		string imageName = "4x4Marker_";
 		convert << imageName << i << ".jpg";
-		imwrite(convert.str(),outputMarker);
+		imwrite(convert.str(), outputMarker);
 
+	}
+}
+
+void createKnowBoardPosition(Size boardSize, float squareEdgeLenght, vector<Point3f>& corners)
+{
+	for (int i = 0; i < boardSize.height; i++)
+	{
+		for (int j = 0; j < boardSize.width; j++)
+		{
+			corners.push_back(Point3f(j * squareEdgeLenght, i * squareEdgeLenght, 0.0f));
+		}
+	}
+}
+
+void getChessboardCorners(vector<Mat> imgs, vector<vector<Point2f>>& allFoundConers, bool showResults)
+{
+	for (vector<Mat>::iterator iter = imgs.begin(); iter != imgs.end(); iter++)
+	{
+		vector<Point2f> pointBuf;
+		bool found = findChessboardCorners(*iter, Size(9, 6), pointBuf, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_NORMALIZE_IMAGE);
+
+		if (found)
+		{
+			allFoundConers.push_back(pointBuf);
+		}
+
+		if (showResults)
+		{
+			drawChessboardCorners(*iter, Size(9, 6), pointBuf, found);
+			imshow("Lookin for Corners", *iter);
+			waitKey(0);
+		}
 	}
 }
 
 int main(int arg, char** argc)
 {
-	createArucoMarkers();
+	Mat frame;
+	Mat drawToFrame;
 
+	Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
+
+	Mat distanceCoefficients;
+
+	vector<Mat> savedImages;
+
+	vector<vector<Point2f>> markerCorners, rejectedCandidates;
+
+	VideoCapture vid(0);
+
+	if (!vid.isOpened())
+	{
+		return 0;
+	}
+
+	int fps = 20;
+
+	namedWindow("Webcam", CV_WINDOW_AUTOSIZE);
+
+	while (true)
+	{
+		if (!vid.read(frame))
+			break;
+		
+		vector<Vec2f> foundPoints;
+		bool found = false;
+
+		found = findChessboardCorners(frame, chessboardDimensions, foundPoints, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_NORMALIZE_IMAGE );
+		frame.copyTo(drawToFrame);
+		drawChessboardCorners(drawToFrame, chessboardDimensions, foundPoints, found);
+
+		if (found)
+			imshow("Webcam", drawToFrame);
+		else
+			imshow("Webcam", frame);
+		char character = waitKey(1000 / fps);
+	}
+
+	return 0;
 }
